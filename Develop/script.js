b68624d2158c4a045ad3c41b16725211d360ec6a
Inna 1201 <inna.ogurcova@gmail.com>
2022-07-20 09:31:21 +0100

    javascript amended

--- Develop/script.js
+++ Develop/script.js
@@ -2,56 +2,120 @@
 var generateBtn = document.querySelector("#generate");
 
 //I used array to store necessary data
-var lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var uppercasecharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","Z","W","X","Y","Z"];
-var numericRange = ["0","1","2","3","4","5","6","7","8","9"];
-var specialCharacters = ["/","[","]","!","@","#","$","%","^","&","*","(",")","_","+",";","'",":",",",".","<",">","?","+","-","~"];
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "Z", "W", "X", "Y", "Z"];
+var numericRange = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["/", "[", "]", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", ";", "'", ":", ",", ".", "<", ">", "?", "+", "-", "~"];
 
-// Used to empty space
-var passwordValue = "";
+// ??????
+var passwordValue = [];
 
 function generatePassword() {
-var passwordLengt = prompt("Please choose the length of the password you want to generate. Password must be between 8-128 characters.");
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("Password must contain at least 8 characters and no more than 128 characters!");
-  return passwordValue; 
-}
-
-}
-
-
-
-
-
-
-/* function userOptions() {
-var passwordLengt = prompt("Please choose the length of the password you want to generate. Password must be between 8-128 characters.");
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("Password must contain at least 8 characters and no more than 128 characters!");
-  // return to question??????????????????
-}
-
-if (passwordLength > 8 || passwordLength < 128) {
-  console.log (lowercaseCharactersUse)
-  // how to make it to rememeber the lenght for password generation???????????????????
-}
-
-
-var lowercaseCharactersUse = prompt("Do you want to include lowercase characters? (Please enter yes or no) ");
-if (lowercaseCharactersUse = "no"){
-  console.log (uppercaseCharactersUse)
-  // how to make it skip this step?????????????????????
+  //used to generate message in promtbox
+  var passwordLength = prompt("Please choose the length of the password you want to generate. Password must be between 8-128 characters.");
+  // Error message
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must contain at least 8 characters and no more than 128 characters!");
+    return;
+  }
+  // used to log the choice
+  if (passwordLength > 8 || passwordLength < 128) {
+    console.log(passwordLength)
   }
 
 
-if (lowercaseCharactersUse = "yes"){
-console.log (uppercaseCharactersUse)
-// how to make it to generate and remember????????????????????
+  var lowercaseCharactersUse = confirm("Do you want to include lowercase characters?");
+  // used to log the coice
+  if  (lowercaseCharactersUse) {
+    console.log(true);
+  // used to generate random character
+    passwordValue.push(lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)])
+    console.log(passwordValue)
+  } else {
+    console.log(false);
+  }
+
+
+  var uppercaseCharactersUse = confirm("Do you want to include uppercase characters?");
+  // used to log the coice
+  if  (uppercaseCharactersUse) {
+    console.log(true);
+    // used to generate random character
+    passwordValue.push(uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)])
+    console.log(passwordValue)
+  } else {
+    console.log(false);
+  }
+  
+  var numericRangeUse = confirm("Do you want to include numbers?");
+  // used to log the coice
+  if  (numericRangeUse) {
+    console.log(true);
+    // used to generate random character
+    passwordValue.push(numericRange[Math.floor(Math.random() * numericRange.length)])
+    console.log(passwordValue)
+  } else {
+    console.log(false);
+  }
+
+  var specialCharactersUse = confirm("Do you want to include special characters?");
+  // used to log the coice
+  if  (specialCharactersUse) {
+    console.log(true);
+  // used to generate random character
+    passwordValue.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)])
+    console.log(passwordValue)
+  } else {
+    console.log(false);
+  }
+
+//console.log(parseInt(passwordLength))
+
+//console.log(passwordValue.length)
+
+//console.log(typeof (parseInt(passwordLength) - passwordValue.length))
+
+// var allCharacters = [].concat(lowercaseCharacters, uppercaseCharacters, numericRange, specialCharacters);
+
+// console.log(allCharacters)
+
+  for (var i=0; i < ((parseInt(passwordLength) - passwordValue.length)); i++) {
+    if (lowercaseCharactersUse === true) {
+      passwordValue.push(lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)]) 
+      console.log(i)
+    }
+  }
+
+  // for (var i=0; i < ((parseInt(passwordLength) - passwordValue.length)); i++) {
+  //   if (uppercaseCharactersUse === true) {
+  //     passwordValue.push(uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)]) 
+  //     console.log(i)
+  //   }
+  // }
+
+  // for (var i=0; i < ((parseInt(passwordLength) - passwordValue.length)); i++) {
+  //   if (numericRangeUse === true) {
+  //     passwordValue.push(numericRange[Math.floor(Math.random() * numericRange.length)]) 
+  //     console.log(i)
+  //   }
+  // }
+
+  // for (var i=0; i < ((parseInt(passwordLength) - passwordValue.length)); i++) {
+  //   if (specialCharactersUse === true) {
+  //     passwordValue.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]) 
+  //     console.log(i)
+  //   }
+  // }
+ 
+
+console.log(passwordValue)
+
+// change passwordValue from array to string before using return (using info from w3schools JavaScript Array toString() )
+
+  return passwordValue;
 }
 
-var uppercaseCharactersUse = prompt("Do you want to include uppercase characters? (Please enter yes or no)");
 
-var specialCharacterChoice = prompt("Do you want to include a special character? (Please enter yes or no)");*/
 
 
 
